1d43608c3566f77d5dc280a59b3c3ca598d451f9
dedos95 <31134579+dedos95@users.noreply.github.com>
2017-10-12 16:08:11 -0300

    first commit
    adding the files

---
+++ monitoreoPython/src/test/java/edu/usach/monitoreoPython/MonitoreoPythonApplicationTests.java
@@ -0,0 +1,16 @@
+package edu.usach.monitoreoPython;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MonitoreoPythonApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
