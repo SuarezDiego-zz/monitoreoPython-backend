1e61a3536c1cdb0ca15d7acdb7a423c721e3f3cb
dedos95 <31134579+dedos95@users.noreply.github.com>
2017-10-12 16:14:53 -0300

    fixing up the first commint
    copying the files in the main folder

---
+++ src/main/java/edu/usach/monitoreoPython/MonitoreoPythonApplication.java
@@ -0,0 +1,12 @@
+package edu.usach.monitoreoPython;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonitoreoPythonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonitoreoPythonApplication.class, args);
+	}
+}
